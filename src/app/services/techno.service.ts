1ff1f0d38abe7a5e3f1ca0417a430dcb3471d095
Romain_Perdriat <romain@DESKTOP-H15MTN6>
2022-05-05 11:43:46 +0200

    service try with array for new Tech

---
+++ src/app/services/techno.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TechnoService {
+  //technos n'a pas besoin d'être visible de l'extérieur donc je le met en private
+ private technos = [];
+  // Ici, j'initialise un tableau vide car au début je n'ai pas de technos
+
+  constructor() { }
+ //Ensuite je crée la méthode qui va venir ajouter dans ce tableau vide ma nouvelle techno.
+  createTechno(techno) {
+    this.technos = [techno, ...this.technos];
+    console.log (`tableau après ajout de ma tech`, this.technos)
+  }
+  // Pour ce faire, je n'oublie pas d'utiliser le this, je récupère avec le spread operator tout ce qu'il y avait avant dans le tableau et ensuite, je place devant ma nouvelle techno.
+
+}
--- src/app/techno-add/techno-add.component.ts
+++ src/app/techno-add/techno-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TechnoService } from '../services/techno.service';
 
 @Component({
   selector: 'app-techno-add',
@@ -6,13 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./techno-add.component.css']
 })
 export class TechnoAddComponent implements OnInit {
-
-  constructor() { }
+ // Je crée mon instance de service, c'est a dire une instance de technoservice que j'ai définit dans ma classe techno.service.ts
+ //Je définis l'accesseur, ici ce sera private car je n'ai pas besoin de mon instance en dehors de ma classe, pas besoin dans mon html. Je la nomme technoService et je lui donne le type  TechnoService qui renvoie en fait à la classe utilisé pour l'initier.
+ // Donc la structure c'est : accesseur + nom + Type (importer automatiquement pour nous qui pointe le service ou est notre classe)
+  constructor( private technoService : TechnoService) { }
 
   ngOnInit(): void {
   }
 
   addTechno(form) {
-    console.log(form.value)
+    // console.log(form.value);
+    this.technoService.createTechno(form.value)
+    // Ici avec le this je peux aller cibler mon instance qui contient grâce à mon injection createTechno.
+    //Cette méthode attendant un argument, je lui passe la value de mon form
   }
 }
